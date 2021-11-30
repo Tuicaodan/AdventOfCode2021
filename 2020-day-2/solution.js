98d74a94b81848b8fd076f97da1aa0c751663bd1
tuicaodan <tuicaodanad@gmail.com>
2021-11-30 21:26:23 +1300

    add 2020 day 2 solution

---
+++ 2020-day-2/solution.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function () {
+  let count = 0;
+
+  input.forEach((element) => {
+    const regex =
+      /(?<lowBound>\d+)-(?<highBound>\d+)\s(?<targetWord>\w):\s(?<password>\w+)/g;
+
+    const regexResult = regex.exec(element);
+    const lowBound = regexResult.groups.lowBound;
+    const highBound = regexResult.groups.highBound;
+    const targetWord = regexResult.groups.targetWord;
+    const password = regexResult.groups.password;
+
+    if (passPolicy(lowBound, highBound, targetWord, password)) {
+      count++;
+    }
+  });
+
+  const resultOutput = this.document.querySelector(".result");
+  resultOutput.innerHTML = `${count}`;
+});
+
+const passPolicy = (lowBound, highBound, targetWord, password) => {
+  let result = false;
+  const NumTargetwordInPassword = password.split(targetWord).length - 1;
+  if (
+    NumTargetwordInPassword >= lowBound &&
+    NumTargetwordInPassword <= highBound
+  ) {
+    result = true;
+  }
+  return result;
+};
